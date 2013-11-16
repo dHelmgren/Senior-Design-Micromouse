403bb8ea83ff24f59a5d6737b7370e75034f86ef
Emilia Holbik <emiliaholbik@gmail.com>
2013-11-16 14:04:29 -0800

    Added stub code for the actual mouse

---
+++ Code/MplabBench/decisionMaker.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "decisionMaker.h"
+
+void minitaurMain(float leSenCl, float strSenCl, float riSenCl){
+
+}
---
+++ Code/MplabBench/decisionMaker.h
@@ -0,0 +1,6 @@
+#ifndef __MINITAUR
+#define __MINITAUR
+
+void minitaurMain(float leSenCl, float strSenCl, float riSenCl);
+
+#endif
