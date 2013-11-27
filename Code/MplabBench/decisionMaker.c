fc02bcb6da4d1d5457aea7eb5e86044007e922f5
Emilia Holbik <emiliaholbik@gmail.com>
2013-11-27 11:14:28 -0800

    Reorganized file structure

--- Code/MplabBench/decisionMaker.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "decisionMaker.h"
-
-void minitaurMain(float leSenCl, float strSenCl, float riSenCl){
-
-}
--- Code/MplabBench/decisionMaker.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MINITAUR
-#define __MINITAUR
-
-void minitaurMain(float leSenCl, float strSenCl, float riSenCl);
-
-#endif
