2b0c8ebc046ec0581eeb23731aaac05972a9c043
Emilia Holbik <emiliaholbik@gmail.com>
2014-01-05 23:08:20 -0800

    Updated Devon's optical encoder I/O code, including new feature of polled clicks for encoders packaged into functions

--- Code/MplabBench/SensorEncoderIO/Cexample.c
+++ Code/MplabBench/SensorEncoderIO/Cexample.c
@@ -11,6 +11,39 @@
 #define false   0 
 ///end copy
 
+void initial(void);
+void initTimers(void);
+void getClicks(void);
+void clearClicks(void);
+void pollClicks(void);
+void initIR1(void);
+void pollIR1(void);
+
+// NOTE: MPLAB seems to need its declared variables to come first
+/*
+	NOTE: Comments in this code of the style seen here are used
+	to note that there is a commented-out BRA instruction
+	which is not desired for every simulation but will be necessary
+	for Microtaur to function properly.
+*/
+
+void main(void) {
+
+		initial();
+		initTimers();
+		getClicks();
+		pollClicks();
+		initIR1();
+		pollIR1();
+}
+
+//
+// Functions
+//
+
+/*
+	Initialization code provided by Janel
+*/
 void initial(void)
 {
 	//set up the internal oscillator to 8MHz
@@ -35,48 +68,13 @@ void initial(void)
 	RB3 = 0b0;
 }
 
-void main(void) {
-		
-// for reasons unknown to me, MPLAB wanted these
-// variable instansiations to be on top.
-        char distL0;
-        char distL1;
-        char distR0;
-        char distR1;
-
-        char dist0;
-        char dist1;
-
-		initial();
+/*
+	Initialize the timers to count the pulses
+	for going forward or backward
+*/
+void initTimers(void)
+{
 		_asm
-		CLRF distL0,0
-		CLRF distL1,0
-		CLRF distR0,0
-		CLRF distR1,0
-		CLRF dist0,0
-		CLRF dist1,0
-		_endasm
-
-// Set up the configure bits as instructed by Janel:
-//		_asm
-//		MOVLW 0xAC
-//		MOVWF 0x1FFF8,0
-//		MOVLW 0xF7
-//		MOVWF 0x1FFF9,0//problem
-//		MOVLW 0xFB
-//		MOVWF 0x1FFFA,0
-//		MOVLW 0xFF
-//		MOVWF 0x1FFFB,0
-//		MOVWF 0x1FFFC,0
-//		MOVWF 0x1FFFD,0
-//		MOVWF 0x1FFFE,0
-//		MOVWF 0x1FFFF,0
-//		_endasm
-
-// Code to set up the CCP6 and CCP7 to capture the pulse values entering RA6 and RA7 from the encoder.
-// assume that the CCP are at RA6 for left and RA7 for right encoder.
-
-        _asm
 		// set up the proper ports to be inputs
         BSF TRISC, 0,0                //RC0 is now an input
     	BSF TRISC, 1,0                //RC1 is now an input		
@@ -86,20 +84,7 @@ void main(void) {
 		CLRF TMR1H,0
 		CLRF TMR3L,0
 		CLRF TMR3H,0
-/*
-		// clear the registers which will be used to 
-		// count the pulses
-		CLRF CCPR4L,0
-		CLRF CCPR4H,0
-		CLRF CCPR7L,0
-		CLRF CCPR7H,0
-*/
-/*
-		// capture on falling edge
-        MOVLW 0b00000100        
-        MOVWF CCP7CON,0
-        MOVWF CCP4CON,0                 //pg.257 from text
-*/
+
 		// RC0 == T1CKI, so no remapping is necessary
 		// RC1 != T3CKI && RC1 == RP12, so remap T3CKI to RP12
 		MOVLW 0b00001100			//setting RP12 as Timer3
@@ -123,106 +108,84 @@ void main(void) {
 									//Enables register read/write of timer in one 16-bit operationerations
 									//Enables timer3
 		MOVWF T3CON,0
+		_endasm
+}
 
-		///testing
-		// toggle RC0 and RC1 a few times
-		BSF PORTC,0,0
-		BCF PORTC,0,0
-		BSF PORTC,0,0
-		BCF PORTC,0,0
-		BSF PORTC,0,0
-		BCF PORTC,0,0
-		BSF PORTC,0,0
-		BCF PORTC,0,0
+/*
+	Move the contents of the timers to respective variables.
+*/
+void getClicks(void)
+{
+// Here is the code to capture the values
+// The values in WREG may then be moved to the appropriate location(s),
+// which is likely hardcoded, before the next value is loaded to WREG.
+// Here we just override sampleAddress four times
 
-		BSF PORTC,1,0
-		BCF PORTC,1,0
-		BSF PORTC,1,0
-		BCF PORTC,1,0
-		BSF PORTC,1,0
-		BCF PORTC,1,0
+		char sampleAddress = 0;
 
-INFIN:	NOP
-        // here is the code to capture the values
+		_asm
+L1:		
 		// the left encoder
-		MOVF TMR1L,0xE8,0//address FE8 == WREG
-		MOVWF distL0,0
+ 		MOVF TMR1L,0xE8,0//address FE8 == WREG
+		MOVWF sampleAddress,0
 		MOVF TMR1H,0xE8,0
-		MOVWF distL1,0
+		MOVWF sampleAddress,0
 		// the right encoder
 		MOVF TMR3L,0xE8,0
-		MOVWF distR0,0
+		MOVWF sampleAddress,0
 		MOVF TMR3H,0xE8,0
-		MOVWF distR1,0
-
-		BRA INFIN
+		MOVWF sampleAddress,0
 /*
-        MOVFW TMR1L
-		MOVWF distL0,0
-/*              //
-        MOVFF TMR1H, distL1                // the left encoder
-        MOVFF TMR3L, distR0                //
-        MOVFF TMR3H, distR1                // the right encoder
-/*
-		// set timer1 to ccp7
-		// set timer3 to ccp4
-        MOVLW 0b00000010
-        MOVWF CCPTMRS1,0                //pg.258 of text, pg.261 of text
+		BRA L1
 */
-/*   /// old code /// 
-		// initialize timer0
-		MOVLW 0b10111000
-									//timer0 enabled
-									//16bit
-									//external
-									//falling edge
-									//no prescaler
-		MOVWF T0CON,0
+		_endasm
+}
 
-		// initialize timer1
-		MOVLW 0b10000101
-									//external
-									//1:1 prescale value
-									//external
-									//do not synchronize external clock input
-									//read write of timer1 in two 8bit operations
-									//timer1 enabled
-		MOVWF T1CON,0
+/*
+	Clear the timers.
+*/
+void clearClicks()
+{
+// clear timers 1 and 3
+		_asm
+		CLRF TMR1L,0
+		CLRF TMR1H,0
+		CLRF TMR3L,0
+		CLRF TMR3H,0
+		_endasm
+}
 
-		// remap RC1 to T0CKI
-		MOVLW 0b00001100			//setting RP12 as Timer0
-		MOVWF RPINR4,0				//remap. pg164
+/*
+	Clear the timers and watch for the timers to reach
+	a pre-set value. Note that this code is only set to watch
+	the low byte of each timer.
+*/
+void pollClicks(void)
+{
+		clearClicks();
+		_asm
+		MOVLW 25// some constant which will have to be determined by measurement
+L1:		CPFSGT TMR1L,0
+/*
+		BRA L1
+*/
+// we may choose to only moniter one of the encoders for simplicity, but both are coded here
+L2:		CPFSGT TMR3L,0
+/*
+		BRA L2
+*/
+		_endasm	
+		// both of the timers are greater than the hardcoded constant
+}
 
-        MOVLW 0b00000000        //pg258 from text.
-                                //CCP7 is based off of TMR1/TMR2
-                                //CCP6 is based off of TMR1/TMR2
-
-        MOVWF CCPTMRS1,0                //
-        MOVWF CCPTMRS2,0                //
-        MOVLW 0b00000100        //pg257 from text.
-                                //capture on falling edge
-
-        MOVWF CCP7CON,0                //
-        MOVWF CCP6CON,0                //
-
-								// clear the registers which will be used to 
-								// count the pulses
-		CLRF CCPR6L,0
-		CLRF CCPR6H,0
-		CLRF CCPR7L,0
-		CLRF CCPR7H,0
-*/ /// end old code ///
-
-        _endasm
-
-///////////////////////////////////////////////////////////////////////////////////////
-//Code to recieve data from a sensor and pass it to stand-in variables DIST0 and DIST1
-//Let's pretend that Vo of the sensor is connected to RA2 of the PIC.
-
-
-
-        _asm
-        BSF TRISA, 1,0                //RA1 is now an input
+/*
+	Set up one IR and initialize the ADC. This code assumes that
+	one of the IRs is connected to RA1.
+*/
+void initIR1(void)
+{
+		_asm
+		BSF TRISA, 1,0          //RA1 is now an input
         MOVLW 0b11000101        //pg507 from text.
                                 //internal RC used for clock source
                                 //CHAN0 selected
@@ -235,18 +198,19 @@ INFIN:	NOP
                                 //all ANx ports set to recieve analog in
                                 //Vdd set to Vref+ and Vss set to Vref-
         MOVWF ADCON1,0                //
+		_endasm
+}
 
-        //at this point the set up should be complete. The set up will need to be
-        //modified for the actual PIC
-        
+void pollIR1(void)
+{
+        char dist0 = 0;
+        char dist1 = 0;
 
-REPEAT: BSF ADCON0, 1,0                //start the conversion
-WAIT:   BTFSC ADCON0, 1,0        //keep polling for end-of-conversion
-        BRA WAIT                //
-        MOVFF ADRESL, dist0        //
-        MOVFF ADRESH, dist1        //
-        BRA REPEAT                //
-DONE:                        //
-        _endasm
-
+		_asm
+		BSF ADCON0, 1,0         //start the conversion
+WAIT:   BTFSC ADCON0, 1,0       //keep polling for end-of-conversion
+        BRA WAIT               
+        MOVFF ADRESL, dist0     
+        MOVFF ADRESH, dist1               
+		_endasm
 }
